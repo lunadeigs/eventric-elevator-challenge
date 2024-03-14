b26d7714bb0ca324ecf18735813aad47853445c1
Luna Deighan <lunadeighan@gmail.com>
2024-03-14 11:39:30 -0500

    PassengerList and PassengerRow Components (plus dummy data for them

--- app/passengerList.tsx
+++ app/passengerList.tsx
@@ -1,13 +1,44 @@
 'use client'
 
-function PassengerRow(){
-    return <div>
-        <p>Placeholder</p>
-    </div>
+import CardWrapper from "./cardWrapper"
+
+function PassengerRow(props:{
+    name:string,
+    floor:number
+}){
+    return <tr>
+        <td className="w-10 text-capitalize">{props.name}</td>
+        <td className="w-2">{props.floor}</td>
+    </tr>
 }
 
-export default function PassengerList(){
-    return <div>
-        <p>Placeholder</p>
-    </div>
+export default function PassengerList(props:{
+    elevatorPassengers: {
+        name: string,
+        floor: number
+    }[]
+}){
+    return <CardWrapper col={4}>
+        <div className="card-header">
+            <h3 className="card-title">Passenger List</h3>
+        </div>
+        <div className="card-table table-responsive">
+            <table className="table table-vcenter">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Floor</th>
+                    </tr>
+                </thead>
+                <tbody>{
+                    props.elevatorPassengers.map((val, inx) => {
+                        return <PassengerRow
+                            key={val.name}
+                            {...val}
+                        />
+                    })
+                }</tbody>
+            </table>
+        </div>
+    </CardWrapper>
 }
